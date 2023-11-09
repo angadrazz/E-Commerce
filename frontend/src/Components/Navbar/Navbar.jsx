a7e9b236243aa608e315233e81e9ed16ec726fcd
angadrazz <angadraj9534@gmail.com>
2023-11-09 17:49:06 +0530

    added some code

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -6,7 +6,16 @@ import cart_icon from '../Assests/cart_icon.png'
 const Navbar = () => {
     return (
         <div className="navbar">
-
+        <div className="nav-logo">
+            <img src={logo} alt="" />
+            <p>SHOPPER</p>
+        </div>
+        <ul className="nav-menu">
+            <li>Shop</li>
+            <li>Men</li>
+            <li>Women</li>
+            <li>Kids</li>
+        </ul>
         </div>
     )
 }
