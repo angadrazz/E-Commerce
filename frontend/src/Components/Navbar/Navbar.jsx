04b1a7f2e7e675cc3be67e53b3bf07519eadf7cb
angadrazz <angadraj9534@gmail.com>
2023-11-09 18:03:06 +0530

    javascript js navbar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -16,6 +16,10 @@ const Navbar = () => {
             <li>Women</li>
             <li>Kids</li>
         </ul>
+        <div className="nav-login-cart">
+            <button>Login</button>
+            <img src={cart_icon} alt="cart_icon" />
+        </div>
         </div>
     )
 }
